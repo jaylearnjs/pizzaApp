d2b4fd3c11ef62093dfdbd9fbd490c826aabf020
jaylearnjs <jaylearnjs@gmail.com>
2022-08-25 20:22:39 +0530

    cart working

--- src/cart.js
+++ src/cart.js
@@ -17,6 +17,38 @@ const Cart = (props) => {
           return (
             <>
               <div className="wholeItemDiv">
+                <div className="imgName">
+                  <div className="itemImg">
+                    {/* <img src={indData.pizimage} /> */}
+                  </div>
+                  <div className="pizNameSize">
+                    <h1>{indData.name}</h1>
+                    <div>{indData.description}</div>
+                    <div className="sizenCrust">
+                      <div>{indData.size} | </div>
+                      <div>{indData.crust}</div>
+                    </div>
+                  </div>
+                </div>
+                <div className="pricenQty">
+                  <div>
+                    <span className="qty">-</span>
+                    <span className="qty">1</span>
+                    <span className="qty">+</span>
+                  </div>
+                  <div>₹ {indData.price}</div>
+                </div>
+                <div className="CustData">
+                  <div className="custHeading">Your Customisation</div>
+
+                  {indData.toppings.map((conCurTop) => {
+                    return (
+                      <>
+                        <span>{conCurTop}</span>
+                      </>
+                    );
+                  })}
+                </div>
                 {/* {" "}
                 <div>{indData.name}</div>
                 <div>{indData.size}</div>
